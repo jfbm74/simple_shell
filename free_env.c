9d4499b1d784272f2e07961fca078e674c768afc
Ricardo Hincapie <1516@holbertonschool.com>
2020-04-17 04:17:58 +0000

    Add free env

---
+++ free_env.c
@@ -0,0 +1,16 @@
+#include "hsh.h"
+/**
+* free_env - frees tokenized PATH directories and user's arguments
+* @env_args: tokenized environment directories
+* @args: tokenized user's commands
+* Return: nothing
+*/
+
+void free_env(char **env_args, char **args)
+{
+	if (env_args != NULL)
+	{
+		freedom(2, env_args);
+	}
+	freedom(2, args);
+}
