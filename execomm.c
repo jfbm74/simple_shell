ee5271dc39d621456eedbc190afceffe56bb21ae
jfbm74 <jfbm74@gmail.com>
2020-04-16 23:01:10 -0500

    Addinng funtion to execute user commands

---
+++ execomm.c
@@ -0,0 +1,26 @@
+#include "hsh.h"
+/**
+ * execo - Executes a given command
+ * @args: Given command
+ * Return: Exit = 0 or Error Number
+*/
+int execo(char **args)
+{
+	int status;
+	pid_t pid = fork();
+
+	if (!pid)
+	{
+		if (execve(args[0], args, environ) == -1)
+		{
+			int errcode = errno;
+
+			error_msg(args);
+			exit(errcode);
+		}
+	}
+	else
+		wait(&status);
+	errcode = 0;
+	return (errcode);
+}
