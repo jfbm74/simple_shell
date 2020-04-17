435acd3ecd8435ef4c5612d0eba97965a0e8014f
jfbm74 <jfbm74@gmail.com>
2020-04-16 23:02:17 -0500

    Addinng funtion to search for builtins functions

---
+++ find_builtins.c
@@ -0,0 +1,27 @@
+#include "hsh.h"
+/**
+**find_builtins -  finds wheter user's command is a builtin
+** @user_input: user's command
+**Return: pointer to function builtin or NULL if doesn't exists
+**/
+int (*find_builtins(char *user_input))()
+{
+	int counter = 0, stru_size = 0;
+
+	builtin_struct my_builtins[] = {
+		{"exit", exit_func},
+		{"env", env_func},
+	};
+
+	stru_size = (sizeof(my_builtins) / sizeof(builtin_struct));
+	if (user_input != NULL)
+	{
+		while (counter < stru_size)
+		{
+			if (strcomparer(my_builtins[counter].b_name, user_input) == 0)
+				return (my_builtins[counter].b_func);
+			counter++;
+		}
+	}
+	return (NULL);
+}
