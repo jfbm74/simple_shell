b6061032cf6bb513a9b659cb58a40b1b36acce5d
jfbm74 <jfbm74@gmail.com>
2020-04-16 22:59:57 -0500

    Addinng funtion to print enviroment

---
+++ env_func.c
@@ -0,0 +1,19 @@
+#include "hsh.h"
+/**
+ * env_func - Prints eviroment variables
+ * Return: 0 on success
+*/
+int env_func(void)
+{
+	int counter = 0, lenght = 0;
+
+	while (environ[counter] != NULL)
+	{
+		lenght = strlarge(environ[counter]);
+		write(STDOUT_FILENO, environ[counter], lenght);
+		write(STDOUT_FILENO, "\n", 1);
+		counter++;
+	}
+	write(STDOUT_FILENO, "\n", 1);
+	return (0);
+}
