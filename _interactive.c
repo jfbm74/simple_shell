e676793d4bce4ea60a5d8a9e8be2649ae5317312
Ricardo Hincapie <1516@holbertonschool.com>
2020-04-19 22:15:38 +0000

    Add interactive

--- _interactive.c
+++ _interactive.c
@@ -12,7 +12,8 @@ int _interactive(char **av __attribute__((unused)))
 	int (*b_func)() = NULL;
 	size_t bufsiz;
 	char *buffer = NULL, **args = NULL, **env_args = NULL;
-
+	char *full_command = NULL;
+	
 	while (1)
 	{
 		write(STDOUT_FILENO, "($) ", 4);
@@ -20,40 +21,109 @@ int _interactive(char **av __attribute__((unused)))
 		if (readbuffer == EOF)
 		{
 			write(STDOUT_FILENO, "\n", 1);
-			free(buffer);
-			freedom(2, args);
-			freedom(2, env_args);
-			/*freedom(2, av);*/
-			exit (0);
+			if (buffer != NULL)
+			{
+				free(buffer);
+				buffer = NULL;
+			}
+			
+			
+			if (args != NULL)
+			{
+				freedom(2, args);
+				args =  NULL;
+			}
+			if (env_args != NULL)
+			{
+				freedom(2, env_args);
+				env_args = NULL;
+			}
+			exit(0);
 		}
 		else
 		{
-			
 			if (buffer[0] == 10 || buffer[0] == 9)
 				continue;
 			size = necklace_pearls(buffer);
-			
 			args = parsing(buffer, size);
-			
 			b_func = find_builtins(*args);
-			
 			if (b_func != NULL)
 			{
 				if (b_func == exit_func)
-					free_env(env_args, args);
+				{
+					if (buffer != NULL)
+					{
+						free(buffer);
+						buffer = NULL;
+					}
+					if (args != NULL)
+					{
+						freedom(2, args);
+						args =  NULL;
+					}
+					if (env_args != NULL)
+					{
+						freedom(2, env_args);
+						env_args = NULL;
+					}
+				}
 				b_func();
+				if (buffer != NULL)
+				{
+					freedom(1, buffer);
+					buffer = NULL;
+				}
+
+				if (args != NULL)
+				{
+					freedom(2, args);
+					args = NULL;
+				}
+				continue;
 			}
-			
 			if (flag == 0)
 			{
 				env_args = getenvpath();
 				flag = 1;
 			}
-			args[0] = _insert_path(args, env_args);
 
-			errcode = execo(args);
-			
+			if(args[0] ==  NULL)
+				continue;
+			full_command = _insert_path(args, env_args);
+			if (full_command == NULL)
+			{
+				printf("(%s) COMMAND NOT FOUND\n", args[0]);
+				if (buffer != NULL)
+				{
+					freedom(1, buffer);
+					buffer = NULL;
+				}
+
+				if (args != NULL)
+				{
+					freedom(2, args);
+					args = NULL;
+				}
+				loop++;
+				continue;
+			}
+			else
+			{
+
+				errcode = execo(full_command, args);
+			}
 			loop++;
+			if (buffer != NULL)
+			{
+				freedom(1, buffer);
+				buffer = NULL;
+			}
+			if (args != NULL)
+			{
+				freedom(2, args);
+				args =  NULL;
+			}
+			full_command = NULL;
 		}
 
 	}
