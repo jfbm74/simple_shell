533b208efea11862cdeba53a57d57f1f4e21a588
jfbm74 <jfbm74@gmail.com>
2020-04-19 17:26:12 -0500

    Fixing memory leak issues

--- non_interac_mode.c
+++ non_interac_mode.c
@@ -6,38 +6,70 @@
 */
 int non_interactive_mode(char **av __attribute__((unused)))
 {
+	
 	char *buffer = NULL;
+	char *full_command = NULL;
+	int size, len, flag = 0;
 	size_t bufsiz;
 	char **args = NULL;
-	int size;
-	int len = 0;
 	char **env_args = NULL;
-	int flag = 0;
+	int (*b_func)() = NULL;
 
 	while ((len = getline(&buffer, &bufsiz, stdin)) > 0)
 	{
-		if (buffer[0] == 10 || buffer[0] == 9)
-			continue;
-		size = necklace_pearls(buffer);
-		args = parsing(buffer, size);
-
-		if (strcomparer(args[0], "exit") == 0)
-			exit(0);
-		if (flag == 0)
+		if (len == EOF)
 		{
-			env_args = getenvpath();
-			flag = 1;
+			write(STDOUT_FILENO, "\n", 1);
+			free_all(args, env_args, buffer, NULL);
+			exit(0);
 		}
-		args[0] = _insert_path(args, env_args);
-		execo(args);
-		loop++;
-	}
-	
-	free(buffer);
-	freedom(2, args);
-	freedom(2, env_args);
-	/*freedom(2, av);*/
-	exit(0);
+		else
+		{
+			if (buffer[0] == 10 || buffer[0] == 9)
+				continue;
+			size = necklace_pearls(buffer);
+			args = parsing(buffer, size);
+			b_func = find_builtins(*args);
+			if (b_func != NULL)
+			{
+				if (b_func == exit_func)
+					free_all(args, env_args, buffer, NULL);
+				b_func();
+				free_all(args, NULL, buffer, NULL);
+				continue;
+			}
+			if (flag == 0)
+			{
+				env_args = getenvpath();
+				flag = 1;
+			}
 
+			if (args[0] == NULL)
+				continue;
+			full_command = _insert_path(args, env_args);
+			if (full_command == NULL)
+			{
+				printf("./hsh: 1: %s: not found\n", args[0]);
+				freedom(1, buffer);
+				buffer = NULL;
+				freedom(2, args);
+				args = NULL;
+				loop++;
+				continue;
+			}
+			else
+				errcode = execo(full_command, args);
+			loop++;
+			freedom(1, buffer);
+			buffer = NULL;
+			freedom(2, args);
+			args = NULL;
+			full_command = NULL;
+		}
+	}
+	freedom(1, buffer);
+	buffer = NULL;
+	freedom(2, env_args);
+	env_args = NULL;	
 	return (0);
 }
