641bc655f2cb4ef77a1916ba76a551ea7870a55e
Ricardo Hincapie <1516@holbertonschool.com>
2020-04-20 00:37:17 +0000

    Add hsh.h

--- hsh.h
+++ hsh.h
@@ -11,9 +11,7 @@
 #include <sys/types.h>
 #include <errno.h>
 #include <stdarg.h>
-
 extern char **environ;
-
 /**
 * struct builtin - struct that contains the builtin commands
 * @b_name: name of the command
@@ -24,18 +22,13 @@ typedef struct builtin
 	char *b_name;
 	int (*b_func)();
 } builtin_struct;
-
-/*Global variables*/
 int loop;
 char *hsh_home;
 int errcode;
 char *ptr_dup;
-
-/*Builtin functions*/
 int (*find_builtins(char *user_input))();
 int exit_func(void);
 int env_func(void);
-
 void change_equal_sig(char *str);
 char *ret_path_line();
 void error_msg(char **args);
@@ -50,17 +43,14 @@ char **parsing(char *buffer, int characters);
 int execo(char *command, char **args);
 void free_all(char **dptr1, char **dptr2, char *sptr1, char *sptr2);
 void logo(void);
-/*Utilities*/
 int strncomparer(char *s1, char *s2, size_t n);
 int strlarge(char *s);
 char *strduplicate(char *s);
 int strcomparer(char *s1, char *s2);
 char *strconk(char *dest, char *src);
 int spaces_buster(char *buffer);
-
 int lennum(int n);
 char *int_to_charac(int num);
-
 char *_strcat(char *dest, char *src);
 char *_strdup(char *s);
 char *_itoa(int num);
@@ -69,5 +59,6 @@ int _compare(char *x, char *y);
 int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
 int _strncmp(char *s1, char *s2, size_t n);
+char *_strcpy(char *dest, char *src)
 
-#endif
+#endif /**HOLBERTON_H*/
