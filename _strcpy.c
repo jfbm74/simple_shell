c740e42741f003a98a5aff0cc69c9c9f6a9f8466
Ricardo Hincapie <1516@holbertonschool.com>
2020-04-20 00:40:42 +0000

    Add strcpy

---
+++ _strcpy.c
@@ -0,0 +1,20 @@
+/**
+ * _strcpy - unction that copies the string pointed to by src
+ *@dest: char
+ *@src:_ char
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0, j;
+
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j <= i; j++)
+	{
+		dest[j] = src[j];
+	}
+	return (dest);
+}
